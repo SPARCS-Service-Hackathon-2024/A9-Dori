88cf8c7ce8904bfcf59b314ece2de5cd05d057f8
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-15 14:07:32 +0900

    feat: 기업 측 오퍼 추가 로직 수정

---
+++ src/main/java/com/example/demo/repository/CompanyMatchOfferRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.CompanyMatchOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyMatchOfferRepository extends JpaRepository<CompanyMatchOffer, Long> {
+}
