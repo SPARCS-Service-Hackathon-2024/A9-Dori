4c1af39b9d123e6373528b01fabb0f689c837e57
14Kgun <geon6757@kaist.ac.kr>
2024-02-16 09:18:28 +0900

    Update: Chat

--- web/src/pages/Chat/index.jsx
+++ web/src/pages/Chat/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import AdaptiveCenterDiv from "../../components/AdaptiveDiv/AdaptiveCenterDiv";
 import Footer from "../../components/Footer";
+
+import imgExample from "./imgExample.png";
 
 const Chat = () => {
   return (
     <>
+      <div css={{ height: "50px" }} />
+      <AdaptiveCenterDiv>
+        <Link to="/chat/talk">
+          <img src={imgExample} alt="chat" css={{ width: "100%" }} />
+        </Link>
+      </AdaptiveCenterDiv>
       <Footer />
     </>
   );
